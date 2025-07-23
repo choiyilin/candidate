b6bb96f121b4fa7f4bc8e0d0e596a431bc83bd45
choiyilin <yilinchoi@hotmail.com>
2025-07-22 23:08:29 -0600

    setup work

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 # setup.py
+#still under development
+# This script is used to package the application using py2app.
+# Make sure to run this script in the terminal with the command:
+# python setup.py py2app    
+# 7/22 11:04pm mtn time
 from setuptools import setup
 
 APP = ['main.py']
